982d6fce2759772f83460ec41100cfe7c3d57a8e
Sherk-cell <70703520+Sherk-cell@users.noreply.github.com>
2024-03-26 15:14:21 +0100

    Created a simple player. The player can walk around and look around on a 2D axis

---
+++ PVB Linx Groep 6/Assets/Code/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    [SerializeField]private Rigidbody rb;
+    [SerializeField]private Transform body;
+    [SerializeField] private Camera playerCamera;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        HandlePlayerMovement();
+        HandlePlayerTurning();
+
+    }
+
+  
+
+    private void HandlePlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput,0f ,  verticalInput);
+        
+        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+    
+    private void HandlePlayerTurning()
+    {
+        var (success, position) = GetMousePosition();
+        if (success)
+        {
+            Vector3 direction = position - transform.position;
+            direction.y = 0;
+            body.forward = direction;
+        }
+    }
+    
+    private (bool success, Vector3 position) GetMousePosition()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity))
+            return (success: true, position: hitInfo.point);
+        
+        else
+            return (success: false, position: Vector3.zero);
+    }
+
+}
