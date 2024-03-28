55ac26f0fcad0eef452d0f223c91692917ddcb31
Guidoge3 <70884054+Guidoge3@users.noreply.github.com>
2024-03-28 11:57:46 +0100

    Updated Universal Health script to be only for health from now on
    regeneration script is split away from original commit and removed Enums for regen checks

--- PVB Linx Groep 6/Assets/Code/Scripts/UniversalHealth.cs
+++ PVB Linx Groep 6/Assets/Code/Scripts/UniversalHealth.cs
@@ -3,27 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ObjectType
-{
-    Player,
-    Castle,
-    Enemy
-}
 public class UniversalHealth : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
-    [SerializeField] private int _currentHealth;
-    private bool _isRegenerating = false;
+    private int _currentHealth;
+    
     private bool _didDamage = false;
+    public bool isFullHealth = true;
     private Transform _currentTransform;
-    public ObjectType _objectType;
-    private int _onePercentOfCastleHPInt;
+    
     
     private void Awake()
     {
         _currentHealth = _maxHealth;
-        float onePercentOfCastleHP = _maxHealth * 0.005f;
-        _onePercentOfCastleHPInt = Mathf.RoundToInt(onePercentOfCastleHP);
     }
 
     public void TakeDamage(int amount)
@@ -37,96 +29,18 @@ public class UniversalHealth : MonoBehaviour
     {
         _currentHealth += amount;
         if (_currentHealth >= _maxHealth)
-            _currentHealth = _maxHealth;
-    }
-
-    public void PassiveRegenHealth(int amount)
-    {
-        _currentHealth += amount;
-        if (_currentHealth >= _maxHealth)
-            _currentHealth = _maxHealth;
-    }
-
-    private IEnumerator PassiveRegenDelay(int delayAmount, int amountOfHealth)
-    {
-        if (!_isRegenerating)
         {
-            yield return new WaitForSeconds(delayAmount);
-            PassiveRegenHealth(amountOfHealth);
-            _isRegenerating = false;
-            
-            if (_currentHealth >= _maxHealth - 1)
-            {
-                _isRegenerating = false;
-                StopCoroutine(PassiveRegenDelay(6, 1));
-            }
+            _currentHealth = _maxHealth;
+            isFullHealth = true;
         }
-    }
-
-    private IEnumerator DealingDamageDelay(int delayAmount)
-    {
-        if (!_didDamage)
+        else
         {
-            _didDamage = true;
-            yield return new WaitForSeconds(delayAmount);
-            _didDamage = false;
+            isFullHealth = false;
         }
     }
 
     private void Die()
     {
         Destroy(gameObject);
-    }
-
-    private void Update()
-    {
-        if (_currentHealth <= _maxHealth - 1)
-        {
-            switch (_objectType)
-            {
-                case ObjectType.Player: 
-                    StartCoroutine(PassiveRegenDelay(6, 1));
-                    break;
-                case ObjectType.Castle:
-                    StartCoroutine(PassiveRegenDelay(6, _onePercentOfCastleHPInt));
-                    break;
-                case ObjectType.Enemy:
-                    StartCoroutine(PassiveRegenDelay(6, 1));
-                    break;
-                
-            }
-            _isRegenerating = true;
-        }
-
-        if (_didDamage)
-        {
-            StartCoroutine(DealingDamageDelay(2));
-        }
-        
-        Debug.Log(_currentHealth);
-    }
-    
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        var otherHealth = collision.gameObject.GetComponent<UniversalHealth>();
-        if (otherHealth != null)
-        {
-            switch (_objectType)
-            {
-                case ObjectType.Player:
-                    if (otherHealth._objectType == ObjectType.Enemy)
-                        TakeDamage(20); // Example: Player damages enemy
-                    break;
-                case ObjectType.Castle:
-                    if (otherHealth._objectType == ObjectType.Enemy)
-                        TakeDamage(25); // Example: Castle takes damage from enemy
-                    break;
-                case ObjectType.Enemy:
-                    if (otherHealth._objectType == ObjectType.Player)
-                        TakeDamage(20); // Example: Enemy damages player
-                    break;
-            }
-        }
     }
 }
