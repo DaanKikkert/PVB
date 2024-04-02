b1dda49d51caa7eff4ea20a7799e396b3e35c797
WarlockGames12 <47526227+WarlockGames12@users.noreply.github.com>
2024-04-02 11:44:10 +0200

    It shows health for castle and can be applied for to enemy or player or structures that are build

---
+++ PVB Linx Groep 6/Assets/Code/UI/ShowHealth.cs
@@ -0,0 +1,31 @@
+using Code.Scripts.Health;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI
+{
+    public class ShowHealth : MonoBehaviour
+    {
+
+        [Header("Slider Settings: ")] 
+        [SerializeField] private Slider getSlider;
+        [SerializeField] private DepleteHealth getHealth;
+        [SerializeField] private Text showHealth;
+
+        private int _fixedHealth;
+        
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            getSlider.maxValue = getHealth.health;
+            _fixedHealth = getHealth.health;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            getSlider.value = getHealth.health;
+            showHealth.text = getHealth.health + "/" + _fixedHealth;
+        }
+    }
+}
