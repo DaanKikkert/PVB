0255d50777bd054a996a92bd9bee2186b90f28b2
WarlockGames12 <47526227+WarlockGames12@users.noreply.github.com>
2024-05-16 10:51:00 +0200

    Player can now just shoot instead of just aiming and then shooting

--- PVB Linx Groep 6/Assets/Code/Scripts/Weapons/PlayerAttack.cs
+++ PVB Linx Groep 6/Assets/Code/Scripts/Weapons/PlayerAttack.cs
@@ -1,97 +1,83 @@
 using Code.Scripts.Player;
-using Unity.VisualScripting;
 using UnityEngine;
 using Weapons;
 
-public class PlayerAttack : MonoBehaviour
+namespace Code.Scripts.Weapons
 {
-    
-    [Header("Player Attack Settings: ")]
-    public Weapon weapon;
-    [SerializeField] private Animator playerAttackAnim;
-    
-    [Header("SpawnPoint Settings: ")]
-    [SerializeField] private GameObject spawnPointBow;
-    [SerializeField] private GameObject spawnPointBullet;
-    
-    [Header("Other Script Settings: ")]
-    [SerializeField] private BasicMovement getVector;
-    [SerializeField] private ClassManager playerClass;
-    
-    [Header("Value Changes Settings: ")]
-    [SerializeField] private string attackAnimationName;
-    [SerializeField] [Range(0,1)] private float maxAnimMaskLayer;
-
-    private float _attackTimer;
-    private bool _isHolding;
-
-    private void Start()
+    public class PlayerAttack : MonoBehaviour
     {
-        weapon.SpawnWeapon(spawnPointBow.transform);
-    }
+    
+        [Header("Player Attack Settings: ")]
+        public Weapon weapon;
+        [SerializeField] private Animator playerAttackAnim;
+    
+        [Header("SpawnPoint Settings: ")]
+        [SerializeField] private GameObject spawnPointBow;
+        [SerializeField] private GameObject spawnPointBullet;
+    
+        [Header("Other Script Settings: ")]
+        [SerializeField] private BasicMovement getVector;
+        [SerializeField] private ClassManager playerClass;
+    
+        [Header("Value Changes Settings: ")]
+        [SerializeField] private string attackAnimationName;
+        [SerializeField] [Range(0,1)] private float maxAnimMaskLayer;
 
-    private void Update()
-    {
-        _attackTimer -= Time.deltaTime;
+        private float _attackTimer;
+        private bool _isHolding;
+
+        private void Start()
+        {
+            weapon.SpawnWeapon(spawnPointBow.transform);
+        }
+
+        private void Update()
+        {
+            _attackTimer -= Time.deltaTime;
         
-        switch (playerClass.playerClass)
-        {
-            case PlayerClass.Warrior:
+            switch (playerClass.playerClass)
             {
-                if (Input.GetKey(KeyCode.Mouse0))
-                    Attack();
-                break;
-            }
-            case PlayerClass.Archer:
-                HandleArcherInput();
-                break;
-        }
-        
-        if (playerAttackAnim.GetCurrentAnimatorStateInfo(0).IsName(attackAnimationName) &&
-            playerAttackAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
-        {
-            playerAttackAnim.SetBool("IsAttacking", false);
-            playerAttackAnim.SetLayerWeight(1, 0);
-        }
-    }
-
-    private void HandleArcherInput()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            _isHolding = true;
-            playerAttackAnim.SetBool("IsAiming", true);
-        }
-
-        switch (_isHolding)
-        {
-            case true:
-            {
-                if (Input.GetKeyDown(KeyCode.Mouse0))
-                    Attack();
-            
-                if (Input.GetKeyUp(KeyCode.Mouse1))
+                case PlayerClass.Warrior:
                 {
-                    _isHolding = false;
-                    playerAttackAnim.SetBool("IsAiming", false);
+                    if (Input.GetKey(KeyCode.Mouse0))
+                        Attack();
+                    break;
                 }
-                break;
+                case PlayerClass.Archer:
+                    HandleArcherInput();
+                    break;
+            }
+        
+            if (playerAttackAnim.GetCurrentAnimatorStateInfo(0).IsName(attackAnimationName) &&
+                playerAttackAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                playerAttackAnim.SetBool("IsAttacking", false);
+                playerAttackAnim.SetLayerWeight(1, 0);
             }
         }
-    }
 
-    private void Attack()
-    {
-        if (_attackTimer <= 0f)
+        private void HandleArcherInput()
         {
-            playerAttackAnim.SetLayerWeight(1, getVector.GetDirection().magnitude > 0.01f ? maxAnimMaskLayer : 0f);
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                playerAttackAnim.SetBool("IsAiming", true);
+                Attack();
+            }
+        }
 
-            playerAttackAnim.SetBool("IsAttacking", true);
-            playerAttackAnim.SetBool("IsWalking", false);
+        private void Attack()
+        {
+            if (_attackTimer <= 0f)
+            {
+                playerAttackAnim.SetLayerWeight(1, getVector.GetDirection().magnitude > 0.01f ? maxAnimMaskLayer : 0f);
 
-            weapon.setAttackDirection(spawnPointBullet.transform.rotation, spawnPointBullet.transform.position);
-            weapon.Attack();
-            _attackTimer = weapon.attackDelay;
+                playerAttackAnim.SetBool("IsAttacking", true);
+                playerAttackAnim.SetBool("IsWalking", false);
+
+                weapon.setAttackDirection(spawnPointBullet.transform.rotation, spawnPointBullet.transform.position);
+                weapon.Attack();
+                _attackTimer = weapon.attackDelay;
+            }
         }
     }
 }
